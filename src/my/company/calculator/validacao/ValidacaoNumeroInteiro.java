175a5b8839b37ab04e729117b7b39d609d629ef4
FERNAND FERNANDES OLIVEIRA <fernandfernandesoliveira@gmail.com>
2026-07-07 15:35:08 -0300

    Release v2.3: validação de entradas e repetição de cálculos

--- src/my/company/calculator/validacao/ValidacaoNumeroInteiro.java
+++ src/my/company/calculator/validacao/ValidacaoNumeroInteiro.java
@@ -4,13 +4,23 @@ import java.util.Scanner;
 
 public class ValidacaoNumeroInteiro {
 
-    public static int lerInteiroValido(){
+    public static int lerInteiroValido(String mensagem) {
         Scanner entrada = new Scanner(System.in);
+        int numero;
+        while (true) {
+            System.out.println(mensagem);
+            if (entrada.hasNextInt()) {
+                numero = entrada.nextInt();
+                entrada.nextLine();
+                return numero;
+            } else {
+                System.out.println("Erro, Digite apenas numeros inteiros");
+                entrada.nextLine();
 
+            }
+        }
 
-
-
-        return 0;
     }
 
 }
+
